74a2cdbf3c15125954652b5de3f60e247d3b5480
elGerardo <gerardoyu54@gmail.com>
2023-02-28 20:00:14 -0800

    origina player sprite added, hit on enemy added, player animations added

---
+++ Assets/Scripts/Player/PlayerAtack.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAtack : MonoBehaviour
+{
+    public Transform controlHit;
+    public float radiousHit;
+    public float damageHit;
+    private float timer;
+    private bool canAtack;
+    private Animator Animator;
+    
+    private void Start()
+    {
+        Animator = GetComponent<Animator>();
+        canAtack = true;
+        timer = 0f;
+    }
+
+    private void Update()
+    {
+    
+        if (Input.GetKeyDown("space") && canAtack)
+        {
+            Animator.SetBool("isAtacking", true); 
+            canAtack = false;
+            timer = 0f;
+            Hit();
+        }
+
+        if(!canAtack)
+        {
+            timer += Time.deltaTime; 
+            if (timer >= 0.4)
+            {
+                canAtack = true;
+                Animator.SetBool("isAtacking", false);
+            }
+        }
+
+    }
+
+    private void Hit()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(controlHit.position, radiousHit);
+
+        foreach(Collider2D value in objects)
+        {
+
+            if (value.CompareTag("Enemy"))
+            {
+                Debug.Log("hiting...");
+                //value.transform.GetComponent<Enemy>().TakeHit(damageHit);
+                
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(controlHit.position, radiousHit);
+        Gizmos.DrawWireSphere(controlHit.position, damageHit);
+    }
+}
